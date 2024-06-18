c35001bad02cab20e376e47b0ba439a68ef4a0d8
TGX03 <tgx003@gmail.com>
2024-06-18 14:05:43 +0200

    Implement hashing for Blockdevice

--- Code/src/main/java/edu/kit/unwwi/checkpoints/qemu/models/Blockdevice.java
+++ Code/src/main/java/edu/kit/unwwi/checkpoints/qemu/models/Blockdevice.java
@@ -1,15 +1,30 @@
 package edu.kit.unwwi.checkpoints.qemu.models;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * A class representing any kind of blockdevice,
  * which usually are any form of drive backed by a file on the host.
  */
 public class Blockdevice {
+
+	/**
+	 * The digest to use when computing the hash of the backing file.
+	 * SHA256 by default, can be changed using static method.
+	 */
+	private static MessageDigest DIGEST = DigestUtils.getSha256Digest();
 
 	/**
 	 * The name of this device in QEMU.
@@ -32,6 +47,9 @@ public class Blockdevice {
 	 * The size currently taken up on disk.
 	 */
 	private final long actualSize;
+	private final boolean hasMedia;
+	private final Lock hashLock;
+	private byte[] hash;
 
 	/**
 	 * Create a new Blockdevice.
@@ -43,11 +61,33 @@ public class Blockdevice {
 	 * @param actualSize  The size currently taken up on the host disk.
 	 */
 	public Blockdevice(@NotNull String device, @NotNull String qdev, @Nullable Path path, long virtualSize, long actualSize) {
+
+		//TODO: Is it really a good idea to always compute the hash?
+		this.hasMedia = path != null && Files.exists(path);
+		ReadWriteLock lock = new ReentrantReadWriteLock();
+		this.hashLock = lock.readLock();
+		if (this.hasMedia) {
+			new Thread(() -> {
+				lock.writeLock().lock();
+				try {
+					this.hash = DigestUtils.digest(DIGEST, Files.newInputStream(path));
+				} catch (IOException e) {
+					throw new UncheckedIOException(e);
+				} finally {
+					lock.writeLock().unlock();
+				}
+			}).start();
+		}
+
 		this.device = device;
 		this.qdev = qdev;
 		this.path = path;
 		this.virtualSize = virtualSize;
 		this.actualSize = actualSize;
+	}
+
+	public static void setHashAlgorithm(@NotNull MessageDigest digest) {
+		DIGEST = digest;
 	}
 
 	/**
@@ -99,6 +139,33 @@ public class Blockdevice {
 		return actualSize;
 	}
 
+	/**
+	 * Whether this blockdevice has any medium associated with it,
+	 * e.g. an ISO-File of a drive or a qcow-file.
+	 *
+	 * @return Whether this device has media.
+	 */
+	public boolean hasMedia() {
+		return this.hasMedia;
+	}
+
+	/**
+	 * Returns a hash of the medium associated with this blockdevice.
+	 * The hashing method can be set using setHashAlgorithm.
+	 * All instances of Blockdevice share the same algorithm,
+	 * as one probably wants to compare the files later on.
+	 *
+	 * @return The hash of the associated medium
+	 * @throws IllegalStateException Gets thrown if this device doesn't have any medium associated with it, and therefore doesn't have a hash.
+	 */
+	public byte[] getHash() throws IllegalStateException {
+		if (this.hasMedia) {
+			hashLock.lock();
+			byte[] result = Arrays.copyOf(this.hash, this.hash.length);
+			hashLock.unlock();
+			return result;
+		} else throw new IllegalStateException("This blockdevice has no media, and therefore no associated hash");
+	}
 
 	@Override
 	@NotNull
