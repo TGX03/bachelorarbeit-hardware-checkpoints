5b323ed5a371fa58867fc0c65293a4a258ef9e18
Lars Weber <lars.weber@student.kit.edu>
2024-08-06 14:54:34 +0200

    Use methods with arguments instead of individual implementations in BigByteArrayInputStream

--- Code/src/main/java/edu/kit/unwwi/collections/big/BigByteArrayInputStream.java
+++ Code/src/main/java/edu/kit/unwwi/collections/big/BigByteArrayInputStream.java
@@ -7,7 +7,6 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Arrays;
 
 /**
  * An InputStream backed by a BigArray aka a 2D-array.
@@ -53,12 +52,7 @@ public class BigByteArrayInputStream extends InputStream {
 
 	@Override
 	public int read(byte @NotNull [] buffer) {
-		int copy = buffer.length;
-		if ((long) copy > BigArrays.length(this.array) - position)
-			copy = (int) (BigArrays.length(this.array) - position);
-		BigArrays.copyFromBig(this.array, position, buffer, 0, copy);
-		position += copy;
-		return copy;
+		return read(buffer, 0, buffer.length);
 	}
 
 	@Override
@@ -73,13 +67,7 @@ public class BigByteArrayInputStream extends InputStream {
 
 	@Override
 	public byte[] readAllBytes() {
-		long remaining = BigArrays.length(this.array) - position;
-		int size;
-		if (remaining > Integer.MAX_VALUE) size = Integer.MAX_VALUE;
-		else size = (int) remaining;
-		byte[] result = new byte[size];
-		BigArrays.copyFromBig(this.array, position, result, 0, size);
-		return result;
+		return readNBytes(Integer.MAX_VALUE);
 	}
 
 	@Override
@@ -89,10 +77,10 @@ public class BigByteArrayInputStream extends InputStream {
 
 	@Override
 	public byte[] readNBytes(int length) {
+		if (length > position - BigArrays.length(array) - 1) length = (int) (position - BigArrays.length(array) - 1);
 		byte[] result = new byte[length];
-		int resultingLength = readNBytes(result, 0, length);
-		if (resultingLength == length) return result;
-		else return Arrays.copyOf(result, resultingLength);
+		read(result, 0, length);
+		return result;
 	}
 
 	@Override
